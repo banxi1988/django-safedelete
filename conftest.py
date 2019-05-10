451e0d843854274f1af4c1926f1f329e2831ab4a
banxi1988 <banxi1988@gmail.com>
2019-05-10 15:13:17 +0800

    refactor(tests): 引入 pytest,pytest django,将 test_custom_queryset 改用 pytest 实现

---
+++ conftest.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+import os
+import pytest
+
+__author__ = '代码会说话'
--- safedelete/tests/test_custom_queryset.py
+++ safedelete/tests/test_custom_queryset.py
@@ -2,88 +2,86 @@ from django.db import models
 
 from ..managers import SafeDeleteManager, SafeDeleteQueryset
 from ..models import SafeDeleteModel
-from .testcase import SafeDeleteTestCase
 
+
+import pytest
+pytestmark = pytest.mark.django_db
 
 class CustomQuerySet(SafeDeleteQueryset):
 
-    def green(self):
-        return self.filter(
-            color='green'
-        )
+  def green(self):
+    return self.filter(
+      color='green'
+    )
 
 
 class CustomManager(SafeDeleteManager):
-    _queryset_class = CustomQuerySet
+  _queryset_class = CustomQuerySet
 
-    def green(self):
-        """Implemented here so ``green`` available as manager's method
-        """
-        return self.get_queryset().green()
+  def green(self):
+    """Implemented here so ``green`` available as manager's method
+    """
+    return self.get_queryset().green()
 
 
 choices = (
-    ('red', "Red"),
-    ('green', "Green"),
+  ('red', "Red"),
+  ('green', "Green"),
 )
 
 
 class CustomQuerySetModel(SafeDeleteModel):
-    color = models.CharField(
-        max_length=5,
-        choices=choices
-    )
+  color = models.CharField(
+    max_length=5,
+    choices=choices
+  )
 
-    objects = CustomManager()
+  objects = CustomManager()
 
-    # other manager to test custom QS using ``SafeDeleteManager.__init__``
-    other_objects = SafeDeleteManager(CustomQuerySet)
+  # other manager to test custom QS using ``SafeDeleteManager.__init__``
+  other_objects = SafeDeleteManager(CustomQuerySet)
 
+def test_custom_queryset_original_behavior():
+  """Test whether creating a custom queryset works as intended."""
+  CustomQuerySetModel.objects.create(
+    color=choices[0][0]
+  )
+  CustomQuerySetModel.objects.create(
+    color=choices[1][0]
+  )
 
-class CustomQuerySetTestCase(SafeDeleteTestCase):
+  assert CustomQuerySetModel.objects.count() == 2
+  assert CustomQuerySetModel.objects.green().count() == 1
 
-    def test_custom_queryset_original_behavior(self):
-        """Test whether creating a custom queryset works as intended."""
-        CustomQuerySetModel.objects.create(
-            color=choices[0][0]
-        )
-        CustomQuerySetModel.objects.create(
-            color=choices[1][0]
-        )
+def test_custom_queryset_custom_method():
+  """Test custom filters for deleted objects"""
+  instance = _create_green_instance()
+  instance.delete()
 
-        self.assertEqual(CustomQuerySetModel.objects.count(), 2)
-        self.assertEqual(CustomQuerySetModel.objects.green().count(), 1)
+  deleted_only = CustomQuerySetModel.objects.deleted_only()
 
-    def test_custom_queryset_custom_method(self):
-        """Test custom filters for deleted objects"""
-        instance = self._create_green_instance()
-        instance.delete()
+  # ensure deleted instances available
+  assert deleted_only.count() == 1
 
-        deleted_only = CustomQuerySetModel.objects.deleted_only()
+  # and they can be custom filtered
+  assert deleted_only.green().count() == 1
 
-        # ensure deleted instances available
-        self.assertEqual(deleted_only.count(), 1)
+def test_custom_queryset_without_manager():
+  """Test whether custom queryset may be used without custom manager
+  """
+  instance = _create_green_instance()
+  instance.delete()
 
-        # and they can be custom filtered
-        self.assertEqual(deleted_only.green().count(), 1)
+  # note that ``other_objects`` manager used
+  deleted_only = CustomQuerySetModel.other_objects.deleted_only()
 
-    def test_custom_queryset_without_manager(self):
-        """Test whether custom queryset may be used without custom manager
-        """
-        instance = self._create_green_instance()
-        instance.delete()
+  # ensure deleted instances available
+  assert  deleted_only.count() == 1
 
-        # note that ``other_objects`` manager used
-        deleted_only = CustomQuerySetModel.other_objects.deleted_only()
+  # and they can be custom filtered
+  assert  deleted_only.green().count() == 1
 
-        # ensure deleted instances available
-        self.assertEqual(deleted_only.count(), 1)
-
-        # and they can be custom filtered
-        self.assertEqual(deleted_only.green().count(), 1)
-
-    @staticmethod
-    def _create_green_instance():
-        """Shortcut for creating instance with ``color == green``
-        """
-        return CustomQuerySetModel.objects.create(color=choices[1][0])
+def _create_green_instance():
+  """Shortcut for creating instance with ``color == green``
+  """
+  return CustomQuerySetModel.objects.create(color=choices[1][0])
