733d3b05733d9dcdac017bada74b3bb4a6c3112f
banxi1988 <banxi1988@gmail.com>
2019-05-11 11:11:31 +0800

    refactor(tests): 改造 test_visible_by_pk

--- safedelete/tests/test_visible_by_pk.py
+++ safedelete/tests/test_visible_by_pk.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+from safedelete.tests.asserts import assert_soft_delete
 from ..config import DELETED_VISIBLE_BY_FIELD
 from ..managers import SafeDeleteManager
 from ..models import SafeDeleteModel
-from .testcase import SafeDeleteTestCase
 
 
 class PkVisibleManager(SafeDeleteManager):
@@ -32,58 +32,51 @@ class NameVisibleField(SafeDeleteModel):
     def __str__(self):
         return self.name
 
+import pytest
+pytestmark = pytest.mark.django_db
 
-class VisibilityTestCase(SafeDeleteTestCase):
+@pytest.fixture()
+def instance():
+    return PkVisibleModel.objects.create(
+        name='instance'
+    )
 
-    def setUp(self):
-        self.instance = PkVisibleModel.objects.create(
-            name='instance'
-        )
-        self.namevisiblefield = (
-            NameVisibleField.objects.create(name='NVF 1'),
-            NameVisibleField.objects.create(name='NVF 2'),
-            NameVisibleField.objects.create(name='NVF 3'),
-        )
+@pytest.fixture()
+def namevisiblefield():
+    return (
+        NameVisibleField.objects.create(name='NVF 1'),
+        NameVisibleField.objects.create(name='NVF 2'),
+        NameVisibleField.objects.create(name='NVF 3'),
+    )
 
-    def test_visible_by_pk(self):
-        """Test whether the soft deleted model can be found by filtering on pk."""
-        self.assertSoftDelete(self.instance, save=False)
-        self.assertEqual(
-            PkVisibleModel.objects.filter(
-                pk=self.instance.pk
-            ).count(),
-            1
-        )
-        # Raises PkVisibleModel.DoesNotExist if it isn't found
-        PkVisibleModel.objects.get(
-            pk=self.instance.pk
-        )
 
-    def test_invisible_by_name(self):
-        """Test whether the soft deleted model cannot be found by filtering on name."""
-        self.assertSoftDelete(self.instance, save=False)
-        self.assertEqual(
-            PkVisibleModel.objects.filter(
-                name=self.instance.name
-            ).count(),
-            0
-        )
-        self.assertRaises(
-            PkVisibleModel.DoesNotExist,
-            PkVisibleModel.objects.get,
-            name=self.instance.name
-        )
+def test_visible_by_pk(instance):
+    """Test whether the soft deleted model can be found by filtering on pk."""
+    assert_soft_delete(instance, save=False)
+    assert PkVisibleModel.objects.filter( pk=instance.pk ).count() == 1
+    # Raises PkVisibleModel.DoesNotExist if it isn't found
+    PkVisibleModel.objects.get( pk=instance.pk )
 
-    def test_access_by_passed_visible_field(self):
-        """ Test wether the namefield model can be found by filtering on name. """
-        name = self.namevisiblefield[0].name
-        self.namevisiblefield[0].delete()
-        self.assertRaises(
-            NameVisibleField.DoesNotExist,
-            NameVisibleField.objects.get,
-            pk=self.namevisiblefield[0].id
-        )
-        self.assertEqual(self.namevisiblefield[0], NameVisibleField.objects.get(name=name))
-        cat = NameVisibleField.objects.filter(name=name)
-        self.assertEqual(len(cat), 1)
-        self.assertEqual(self.namevisiblefield[0], cat[0])
+def test_invisible_by_name(instance):
+    """Test whether the soft deleted model cannot be found by filtering on name."""
+    assert_soft_delete(instance, save=False)
+    assert PkVisibleModel.objects.filter( name=instance.name ).count() == 0
+    pytest.raises(
+        PkVisibleModel.DoesNotExist,
+        PkVisibleModel.objects.get,
+        name=instance.name
+    )
+
+def test_access_by_passed_visible_field(namevisiblefield):
+    """ Test wether the namefield model can be found by filtering on name. """
+    name = namevisiblefield[0].name
+    namevisiblefield[0].delete()
+    pytest.raises(
+        NameVisibleField.DoesNotExist,
+        NameVisibleField.objects.get,
+        pk=namevisiblefield[0].id
+    )
+    assert (namevisiblefield[0] == NameVisibleField.objects.get(name=name))
+    cat = NameVisibleField.objects.filter(name=name)
+    assert (len(cat) == 1)
+    assert (namevisiblefield[0] == cat[0])
