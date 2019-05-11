e00f4e8aa055ae526b71291413b3bdd981b76231
banxi1988 <banxi1988@gmail.com>
2019-05-11 22:03:28 +0800

    refactor(tests): 改造 test_many2many

--- safedelete/tests/test_many2many.py
+++ safedelete/tests/test_many2many.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from ..config import DELETED_VISIBLE
 from ..models import SafeDeleteModel
-from .testcase import SafeDeleteTestCase
 
 
 class ManyToManyChild(models.Model):
@@ -17,31 +16,24 @@ class ManyToManyParent(SafeDeleteModel):
     )
 
 
-class ManyToManyTestCase(SafeDeleteTestCase):
+import pytest
+pytestmark = pytest.mark.django_db
 
-    def test_many_to_many(self):
-        """Test whether related queries still works."""
-        parent1 = ManyToManyParent.objects.create()
-        parent2 = ManyToManyParent.objects.create()
-        child = ManyToManyChild.objects.create()
 
-        parent1.children.add(child)
-        parent2.children.add(child)
+def test_many_to_many():
+    """Test whether related queries still works."""
+    parent1 = ManyToManyParent.objects.create()
+    parent2 = ManyToManyParent.objects.create()
+    child = ManyToManyChild.objects.create()
 
-        # The child should still have both parents
-        self.assertEqual(
-            child.parents.all().count(),
-            2
-        )
+    parent1.children.add(child)
+    parent2.children.add(child)
 
-        # Soft deleting one parent, should "hide" it from the related field
-        parent1.delete()
-        self.assertEqual(
-            child.parents.all().count(),
-            1
-        )
-        # But explicitly saying you want to "show" them, shouldn't hide them
-        self.assertEqual(
-            child.parents.all(force_visibility=DELETED_VISIBLE).count(),
-            2
-        )
+    # The child should still have both parents
+    assert child.parents.all().count() == 2
+
+    # Soft deleting one parent, should "hide" it from the related field
+    parent1.delete()
+    assert child.parents.all().count() == 1
+    # But explicitly saying you want to "show" them, shouldn't hide them
+    assert child.parents.all(force_visibility=DELETED_VISIBLE).count() == 2
