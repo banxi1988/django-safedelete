b684d4ef25a486a96828b6bc3f2660d33fcc298a
banxi1988 <banxi1988@gmail.com>
2019-05-11 18:07:20 +0800

    refactor(tests): 改造 test_no_delete

--- safedelete/tests/test_no_delete.py
+++ safedelete/tests/test_no_delete.py
@@ -1,29 +1,30 @@
 from ..config import NO_DELETE
 from ..models import SafeDeleteModel
-from .testcase import SafeDeleteForceTestCase
 
 
 class NoDeleteModel(SafeDeleteModel):
     _safedelete_policy = NO_DELETE
 
 
-class NoDeleteTestCase(SafeDeleteForceTestCase):
+import pytest
+pytestmark = pytest.mark.django_db
 
-    def setUp(self):
-        self.instance = NoDeleteModel.objects.create()
+@pytest.fixture()
+def instance():
+    return NoDeleteModel.objects.create( )
 
-    def test_no_delete(self):
-        """Test whether the model's delete is ignored.
+def test_no_delete(instance):
+    """Test whether the model's delete is ignored.
 
-        Normally when deleting a model, it can no longer be refreshed from
-        the database and will raise a DoesNotExist exception.
-        """
-        self.instance.delete()
-        self.instance.refresh_from_db()
-        self.assertIsNone(self.instance.deleted)
+    Normally when deleting a model, it can no longer be refreshed from
+    the database and will raise a DoesNotExist exception.
+    """
+    instance.delete()
+    instance.refresh_from_db()
+    assert instance.deleted is None
 
-    def test_no_delete_manager(self):
-        """Test whether models with NO_DELETE are impossible to delete via the manager."""
-        NoDeleteModel.objects.all().delete()
-        self.instance.refresh_from_db()
-        self.assertIsNone(self.instance.deleted)
+def test_no_delete_manager(instance):
+    """Test whether models with NO_DELETE are impossible to delete via the manager."""
+    NoDeleteModel.objects.all().delete()
+    instance.refresh_from_db()
+    assert instance.deleted is None
